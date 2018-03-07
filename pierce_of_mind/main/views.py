e0175a218a161e4519e415acc761c291ad4d88b5
glhdavenport <Gracie@Joeys-MacBook-Pro.local>
2018-03-06 21:12:22 -0700

    added template for post - problem with route

--- pierce_of_mind/main/views.py
+++ pierce_of_mind/main/views.py
@@ -5,3 +5,7 @@ from . import main
 @main.route("/")
 def index():
     return render_template("index.html")
+
+@main.route("/post")
+def post():
+    return render_template("post.html", title='title from db', url='video url from db', text='text from db')
