2993db29b57cd50d21f954a93becd4ce1f3ebb0e
Ima Junryou <imajunryou@gmail.com>
2018-04-05 20:25:07 -0600

    Merge Gracie's SQL update

--- pierce_of_mind/sql.py
+++ pierce_of_mind/sql.py
@@ -1,5 +1,5 @@
 # sql.py - Create a SQLite3 DB with tables and dummy data
-
+import datetime
 import sqlite3
 
 with sqlite3.connect("pierceofmind.db") as connection:
@@ -9,7 +9,7 @@ with sqlite3.connect("pierceofmind.db") as connection:
 
     # Create posts table
     c.execute("""CREATE TABLE IF NOT EXISTS posts
-              (post_id INTEGER PRIMARY KEY, title TEXT, video TEXT, post TEXT, author TEXT, pubDate TEXT, modDate TEXT)
+              (post_id INTEGER PRIMARY KEY, title TEXT, video TEXT, post TEXT, author TEXT, pubDate DATE, modDate DATE)
               """)
 
     # Dummy post data
@@ -19,8 +19,8 @@ with sqlite3.connect("pierceofmind.db") as connection:
             "video": "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fmathew.s.pierce.5%2Fvideos%2F10214268153165023%2F&show_text=0&width=560",
             "post": "This is a test of database stuff",
             "author": "Mat",
-            "pubDate": "20-Mar-2018",
-            "modDate": "21-Mar-2018"
+            "pubDate": datetime.date(2018,3,20),
+            "modDate": datetime.date(2018,3,21)
 
         },
         {
@@ -28,13 +28,14 @@ with sqlite3.connect("pierceofmind.db") as connection:
             "video": "https://www.youtube.com/embed/UO3h4FBLWqY",
             "post": "This is a test of more database stuff",
             "author": "Gracie",
-            "pubDate": "20-Mar-2018",
-            "modDate:": "N/A"
+            "pubDate": datetime.date(2018,3,25),
+            "modDate": None
 
         }
     ]
 
     # Insert dummy data into the posts table
-    for post in posts:
-        query = 'INSERT INTO posts VALUES("{title}", "{video}", "{post}", {author}, {pubDate})'
-        c.execute(query.format(**post))
+    for index, post in enumerate(posts):
+        query = 'INSERT INTO posts VALUES(?,?,?,?,?,?,?)'
+        values = (index, post["title"], post["video"], post["post"], post["author"], post["pubDate"], post["modDate"])
+        c.execute(query, values)
