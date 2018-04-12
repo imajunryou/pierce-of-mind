572d33656e7d13713d35f90c7b14ddb789e304c4
Ima Junryou <imajunryou@gmail.com>
2018-04-11 23:55:59 -0600

    Build in SqlAlchemy hookups, including some extremely simple commands for the manager for initializing the database.  Retool index and post to make use of the new style hookup.

--- pierce_of_mind/main/views.py
+++ pierce_of_mind/main/views.py
@@ -1,21 +1,13 @@
-import sqlite3
-from flask import g, render_template
+from flask import render_template
 from . import main
-
-
-def connect_db():
-    """Connect to the local database"""
-    return sqlite3.connect("pierceofmind.db")
+from .models import Post
 
 
 @main.route("/")
 def index():
-    g.db = connect_db()
-
     # Get all post titles and texts
-    cursor = g.db.execute("""SELECT * FROM posts""")
-    posts = [dict(title=row[0], video=row[1], text=row[2]) for row in cursor.fetchall()]
-    g.db.close()
+    p = Post.query.get(1)
+    posts = [dict(id=p.id, title=p.title, video=p.video, text=p.content)]
     return render_template("index.html", posts=posts)
 
 
@@ -26,15 +18,12 @@ def about():
 
 @main.route("post/<int:id>")
 def post(id=None):
-    g.db = connect_db()
-
     # Get post matching the given ID, if any
-    query = 'SELECT * FROM posts WHERE rowid={id}'
-    cursor = g.db.execute(query.format(id=id))
-    rows = cursor.fetchall()
-
-    row = rows[0] if rows else ["This post does not exist", "", ""]
-    post = dict(title=row[0], video=row[1], text=row[2])
+    p = Post.query.get(id)
+    if p is None:
+        post = dict(title="This post does not exist", video="", text="")
+    else:
+        post = dict(title=p.title, video=p.video, text=p.content)
 
     return render_template(
         "post.html",
