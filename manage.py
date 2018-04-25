5ee7d4975d83c45aae7167edee4ec346f84e0e43
Ima Junryou <imajunryou@gmail.com>
2018-04-24 22:02:01 -0600

    Slight cleanup of formatting

--- manage.py
+++ manage.py
@@ -36,12 +36,10 @@ dummy_posts = [
         private=False
     ), Post(
         title="Adventures Among the Roses",
-        content="  ".join(
-            (
-                "This video is about some random thing that was easy to find a url for.",
-                "Enjoy!"
-             )
-        ),
+        content="  ".join((
+            "This video is about a thing that was easy to find a url for.",
+            "Enjoy!"
+        )),
         video="https://www.youtube-nocookie.com/embed/-6Wu0Q7x5D0?rel=0",
         author="cshepard@bruinmail.slcc.edu",
         private=False
@@ -51,12 +49,15 @@ dummy_posts = [
 
 @manager.command
 def recycledb():
-    if prompt_bool("Are you sure you want to replace all of your data with garbage?"):
+    if prompt_bool(
+        "Are you sure you want to replace all of your data with garbage?"
+    ):
         db.drop_all()
         db.create_all()
         for post in dummy_posts:
             db.session.add(post)
         db.session.commit()
+
 
 @manager.command
 def initdb():
